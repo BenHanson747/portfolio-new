582c2a4b70b5ad1255d0a911c1491c11e7456948
Ben Hanson <ben.hanson1991@googlemail.com>
2022-07-09 17:07:21 +0100

    Update

--- src/components/PortfolioData.js
+++ src/components/PortfolioData.js
@@ -3,7 +3,7 @@ const PortfolioData = [
     id: 1,
     src: "./assets/digital-clock.PNG",
     alt: "go to Digital Clock",
-    linksource: "/responsive-clock/build/index.html",
+    linksource: "/responsive-clock/index.html",
     heading: "Digital Clock",
     text: "Built using React.js & CSS3",
   },
